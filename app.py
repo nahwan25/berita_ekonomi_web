f00eca67d5ac33f1f90cc922f642ef43b4b9341c
Nahwan Al Hirzi <161611315+nahwan25@users.noreply.github.com>
2025-07-04 15:12:22 +0700

    Update app.py

--- app.py
+++ app.py
@@ -89,4 +89,7 @@ def download(task_id):
                     headers={"Content-Disposition": f"attachment;filename=berita_{task_id}.csv"})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
